1334023d294fcdc4b0a04b40d016cfe33cd0e364
Alec Diaz <alecdiaz1@yahoo.com>
2022-07-28 15:26:37 -0700

    feat: add icons to listings

--- src/features/listing/components/Listing.tsx
+++ src/features/listing/components/Listing.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 import { ListingProps } from "../types";
 import { FC } from "react";
 import { formatter } from "../utils/formatPrice";
-import PlaceholderImage from "../../../assets/clipboard.svg";
+import Clipboard from "../../../assets/clipboard.svg";
+import Location from "../../../assets/location.svg";
+
+const copyPhoneNumber = (phoneNumber: string) => {
+    // TODO: Show notification that phone number was copied
+    navigator.clipboard.writeText(phoneNumber).then()
+}
 
 export const Listing: FC<ListingProps> = ({
     price,
@@ -24,13 +30,30 @@ export const Listing: FC<ListingProps> = ({
     <p>{title}</p>
     <p>{description}</p>
     { (sellerName && sellerPhoneNumber) && (
-      <p>{`${sellerName} - ${sellerPhoneNumber}`}</p>
+      <p className="flex items-center">
+        {`${sellerName} - ${sellerPhoneNumber}`}
+          <span className="cursor-pointer ml-2 block pt-2">
+            <Image
+              src={Clipboard}
+              alt={sellerPhoneNumber}
+              width="20px"
+              height="20px"
+              className="block"
+              onClick={() => copyPhoneNumber(sellerPhoneNumber)}
+            />
+          </span>
+      </p>
     )}
     { price &&
       <p>{formatter.format(price)}</p>
     }
     { location &&
-      <p>{location}</p>
+      <p className="flex">
+          <span className="mt-0.5 mr-1">
+              <Image src={Location} alt={Location} width="20px" height="20px"/>
+          </span>
+          {location}
+      </p>
     }
   </div>
 )

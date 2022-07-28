5c8143bd878693ed72f42de1991c6bd386fe00cb
Alec Diaz <alecdiaz1@yahoo.com>
2022-07-28 15:59:01 -0700

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => (
+  <div className="bg-fuchsia-200 mt-16 p-4">
+    <h1>:)</h1>
+  </div>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Listings } from "../features/listing/components/Listings";
 import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
 const Index: NextPage = () => (
   <div>
@@ -9,12 +10,13 @@ const Index: NextPage = () => (
       <title>Doll Directory</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
+    <Navbar />
     <main>
-      <Navbar />
       <div className="container mx-auto mt-12">
         <Listings />
       </div>
     </main>
+    <Footer />
   </div>
 )
 

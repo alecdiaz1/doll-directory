f73ecb877e44ab53ecbbee03e340fedce2b4e3ac
Alec Diaz <alecdiaz1@yahoo.com>
2022-07-28 14:31:00 -0700

    chore: update listings types

--- src/features/listing/types/index.tsx
+++ src/features/listing/types/index.tsx
@@ -2,23 +2,27 @@ import { StaticImageData } from "next/image";
 
 export interface ListingProps {
   id: string;
-  price: number;
-  sellerName: string;
-  sellerPhoneNumber: string;
+  price: number | null;
+  sellerName: string | null;
+  sellerPhoneNumber: string | null;
   title: string;
   imageUrl: string | StaticImageData;
   featuredListing: boolean;
+  description: string;
+  location: string | null;
 }
 
 export interface ListingAPIResponse {
   id: string;
-  price: number;
-  sellerName: string;
-  sellerPhoneNumber: string;
+  price: number | null;
+  sellerName: string | null;
+  sellerPhoneNumber: string | null;
   title: string;
   photos: [{
     id: string,
     url: string,
-  }]
+  }] | [];
   featuredListing: boolean;
+  description: string;
+  location: string | null;
 }

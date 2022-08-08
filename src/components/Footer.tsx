5a83aec42fdb271e666bf7fda7a289cc3fda81e4
Alec Diaz <alecdiaz1@yahoo.com>
2022-08-08 16:34:03 -0700

    chore: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
 export const Footer = () => (
-  <div className="bg-fuchsia-200 mt-16 p-4">
-    <h1>:)</h1>
+  <div className="bg-fuchsia-200 mt-16 p-4 flex justify-center">
+    <p>built by&nbsp;
+      <span>
+        <a href="https://twitch.tv/thisisnotbyte" className="underline">thisisnotbyte</a>
+      </span>
+      &nbsp;for nopixel
+    </p>
   </div>
 )

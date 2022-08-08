deef655a2e71128f816e833dced64d301b15b43b
Alec Diaz <alecdiaz1@yahoo.com>
2022-08-08 16:29:19 -0700

    chore: add real logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import DDLogo from "../../public/DDLOGO.png";
+import Link from "next/link";
+
 export const Navbar = () => (
-  <div className="bg-fuchsia-200 p-4 flex justify-center">
-    <h1>Doll Directory</h1>
+  <div className="bg-fuchsia-200 flex justify-center pt-2">
+    <Link href="/">
+      <a>
+        <Image
+          alt="Doll Directory Logo"
+          src={DDLogo}
+          width="80px"
+          height="80px"
+        />
+      </a>
+    </Link>
   </div>
 )

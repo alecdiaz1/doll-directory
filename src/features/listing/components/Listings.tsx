ebffdde91a0948dc817a3ba16b88aca4ec775fc0
Alec Diaz <alecdiaz1@yahoo.com>
2022-08-13 02:21:18 -0700

    feat: add text indicating no listings

--- src/features/listing/components/Listings.tsx
+++ src/features/listing/components/Listings.tsx
@@ -26,46 +26,72 @@ export const Listings = () => {
             </Link>
           </span>
         </p>
-        <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols:1 gap-8">
-          {allListings
-            .filter((listing) => listing.featuredListing)
-            .filter((listing) => new Date(listing.expiryDate) > today)
-            .map((listing) => (
-              <Listing
-                id={listing.id}
-                key={listing.id}
-                price={listing.price}
-                title={listing.title}
-                description={listing.description}
-                sellerName={listing.sellerName}
-                sellerPhoneNumber={listing.sellerPhoneNumber}
-                images={listing.photos}
-                location={listing.location}
-              />
-            ))}
+        <div className={allListings.some(listing => listing.featuredListing)
+          ? "grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols:1 gap-8"
+          : ""
+        }>
+          { allListings.some(listing => listing.featuredListing) ?
+            allListings
+              .filter((listing) => listing.featuredListing)
+              .filter((listing) => new Date(listing.expiryDate) > today)
+              .map((listing) => (
+                <Listing
+                  id={listing.id}
+                  key={listing.id}
+                  price={listing.price}
+                  title={listing.title}
+                  description={listing.description}
+                  sellerName={listing.sellerName}
+                  sellerPhoneNumber={listing.sellerPhoneNumber}
+                  images={listing.photos}
+                  location={listing.location}
+                />
+              ))
+            :
+            <p className="text-lg">No featured listings yet! Contact a&nbsp;
+              <span>
+                <Link href="#footer">
+                  <a className="underline">listing agent</a>
+                </Link>
+              </span> to put your ad here!
+            </p>
+          }
         </div>
       </div>
       <div className="mt-8">
         <h1 className="text-4xl mb-8 font-bold text-center sm:text-left">
           ALL LISTINGS
         </h1>
-        <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols:1 gap-8">
-          {allListings
-            .filter((listing) => !listing.featuredListing)
-            .filter((listing) => new Date(listing.expiryDate) > today)
-            .map((listing) => (
-              <Listing
-                id={listing.id}
-                key={listing.id}
-                price={listing.price}
-                title={listing.title}
-                description={listing.description}
-                sellerName={listing.sellerName}
-                sellerPhoneNumber={listing.sellerPhoneNumber}
-                images={listing.photos}
-                location={listing.location}
-              />
-            ))}
+        <div className={allListings.some(listing => !listing.featuredListing)
+          ? "grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols:1 gap-8"
+          : ""
+        }>
+          { allListings.some(listing => !listing.featuredListing) ?
+            allListings
+              .filter((listing) => !listing.featuredListing)
+              .filter((listing) => new Date(listing.expiryDate) > today)
+              .map((listing) => (
+                <Listing
+                  id={listing.id}
+                  key={listing.id}
+                  price={listing.price}
+                  title={listing.title}
+                  description={listing.description}
+                  sellerName={listing.sellerName}
+                  sellerPhoneNumber={listing.sellerPhoneNumber}
+                  images={listing.photos}
+                  location={listing.location}
+                />
+              ))
+          :
+            <p className="text-lg">No listings yet! Contact a&nbsp;
+              <span>
+                <Link href="#footer">
+                  <a className="underline">listing agent</a>
+                </Link>
+              </span> to put your ad here!
+            </p>
+          }
         </div>
       </div>
     </>
